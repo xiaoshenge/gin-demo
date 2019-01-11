cf24dda7ee9d621208770bbff8841ab8a8b4f988
shenjian <shenjian@yixia.com>
2019-01-11 10:05:17 +0800

    add ip

--- main.go
+++ main.go
@@ -1,11 +1,36 @@
 package main
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"net"
+	"os"
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
+
+
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
+		})
+	})
+	r.GET("/ip", func (c *gin.Context)  {
+		var localIP string
+		host, _ := os.Hostname()
+		addrs, _ := net.LookupIP(host)
+		for _, addr := range addrs {
+			if ipv4 := addr.To4(); ipv4 != nil {
+				if localIP == "" {
+					localIP = fmt.Sprintf("%s", ipv4)
+				} else{
+					localIP = fmt.Sprintf("%s,%s",localIP,ipv4)
+				}
+			}   
+		}
+
+		c.JSON(200, gin.H{
+			"srvIp": localIP,
 		})
 	})
 	r.Run(":9090") // listen and serve on 0.0.0.0:8080
